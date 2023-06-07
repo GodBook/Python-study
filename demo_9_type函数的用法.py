91b255ac148ab96b3f6de837b3de025ecc170406
GodBook <109887947+GodBook@users.noreply.github.com>
2023-06-07 20:31:59 +0800

    Add files via upload

---
+++ demo_9_type函数的用法.py
@@ -0,0 +1,18 @@
+# 使用print函数直接输出数据类型
+print(type('打字'))
+print(type(100))
+print(type(1.22))
+
+# 使用变量存储type语句的结果
+string_type = type('人')
+int_type = type(888)
+float_type = type(1.1)
+print(string_type)
+print(int_type)
+print(float_type)
+
+# 使用type()语句，查看变量中存储的数据类型信息
+name = '人'
+name_type = type(name)
+print(name_type)
+
